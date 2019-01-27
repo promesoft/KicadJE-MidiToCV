05084b9ef81894154c31a1f690abab5c6bcc9b14
promesoft <github@shard.dk>
2019-01-27 12:53:06 +0100

    Arduino code - separate states for all 4 led sections

--- Arduino/AJ_Johansen_Midi2CV_EncoderLED_RevA/AJ_Johansen_Midi2CV_EncoderLED_RevA.ino
+++ Arduino/AJ_Johansen_Midi2CV_EncoderLED_RevA/AJ_Johansen_Midi2CV_EncoderLED_RevA.ino
@@ -35,11 +35,26 @@
 
 boolean A_old=0, B_old=1, SW_old=0;
 boolean left = false, right = false, button = false;
-boolean LED1Data = false, LED2Data = false, LED3Data = false, LED4Data = false, LEDSelData = false, LEDSel2Data = false; 
-unsigned int encoder = 0, buttoncnt = 0;
+
+boolean LEDData[4][4];
+boolean LED1Data = false, LED2Data = false, LED3Data = false, LED4Data = false;
+boolean LEDSelData = false, LEDSel2Data = false; 
+unsigned int encoder[4], buttoncnt = 0, state = 0;
+/* =====================================================
+======================================================*/  
+void setupLEDData(){
+   for (int i=0; i <= 3; i++){
+     for (int j=0; j <= 3; j++){
+      LEDData[i][j]=false;
+     }
+     encoder[i] = 0;
+   }
+}
 /* =====================================================
 ======================================================*/  
 void setup(){
+  setupLEDData();
+  
   pinMode(SWpin, INPUT);
   pinMode(Apin, INPUT);
   pinMode(Bpin, INPUT);
@@ -55,12 +70,7 @@ void setup(){
   pinMode(LEDsel, OUTPUT);
   pinMode(LEDsel2, OUTPUT);
   
-  digitalWrite(LED1, HIGH);
-  digitalWrite(LED2, HIGH);
-  digitalWrite(LED3, HIGH);
-  digitalWrite(LED4, HIGH);
-  digitalWrite(LEDsel, HIGH);
-  digitalWrite(LEDsel2, HIGH);
+  updateLED();
 
   // 1. PCIE1: Pin Change Interrupt Enable 1
   PCICR =  0b00000010; 
@@ -124,20 +134,23 @@ ISR (PCINT1_vect) {
 void checkencoder(){
   if (left){
     left = false;
-    if (encoder != 0) encoder--;
-    updateLEDValue(encoder >> 2);
+    if (encoder[state] != 0) encoder[state]--;
+    updateLEDValue(encoder[state] >> 2);
   }
   if (right){
     right = false;
-    if (encoder < 0x3f) encoder++;
-    updateLEDValue(encoder >> 2);
+    if (encoder[state] < 0x3f) encoder[state]++;
+    updateLEDValue(encoder[state] >> 2);
   }
   if (button){
     button = false;    
     buttoncnt++;
     buttoncnt = buttoncnt & B00000111;
-    LEDSel2Data = buttoncnt & B00000100;
-    LEDSelData = buttoncnt & B00000010;
+    state = buttoncnt >> 1;
+    LEDSel2Data = state & B00000010;
+    LEDSelData = state & B00000001;
+//    LEDSel2Data = buttoncnt & B00000100;
+//    LEDSelData = buttoncnt & B00000010;
     }
 }
 /* =====================================================
@@ -148,28 +161,36 @@ void updateLEDValue(unsigned int nibble)
   if (nibble >= 0x10) nibble = 0x0f;
   else (nibble = nibble & 0x0f);
   if (nibble & 0x01){
-    LED1Data = true;
+//    LED1Data = true;
+    LEDData[state][0] = true;
   }
   else {
-    LED1Data = false;
+//    LED1Data = false;
+    LEDData[state][0] = false;
   }
   if (nibble & 0x02){
-    LED2Data = true;
+//    LED2Data = true;
+    LEDData[state][1] = true;
   }
   else {
-    LED2Data = false;
+//    LED2Data = false;
+    LEDData[state][1] = false;
   }
   if (nibble & 0x04){
-    LED3Data = true;
+//    LED3Data = true;
+    LEDData[state][2] = true;
   }
   else {
-    LED3Data = false;
+//    LED3Data = false;
+    LEDData[state][2] = false;
   }
   if (nibble & 0x08){
-    LED4Data = true;
+//    LED4Data = true;
+    LEDData[state][3] = true;
   }
   else {
-    LED4Data = false;
+//    LED4Data = false;
+    LEDData[state][3] = false;
   }
 
 }
@@ -177,10 +198,14 @@ void updateLEDValue(unsigned int nibble)
 ========================================================
 ======================================================*/ 
 void updateLED(){
-  digitalWrite(LED1, LED1Data);
+/*  digitalWrite(LED1, LED1Data);
   digitalWrite(LED2, LED2Data);
   digitalWrite(LED3, LED3Data);
-  digitalWrite(LED4, LED4Data);
+  digitalWrite(LED4, LED4Data);*/
+  digitalWrite(LED1, LEDData[state][0]);
+  digitalWrite(LED2, LEDData[state][1]);
+  digitalWrite(LED3, LEDData[state][2]);
+  digitalWrite(LED4, LEDData[state][3]);
   delay(1);
   digitalWrite(LEDsel, LEDSelData);
   digitalWrite(LEDsel2, LEDSel2Data);
