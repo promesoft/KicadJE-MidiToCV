d0bc2de94166171eadf7606a713aac5412da8b22
promesoft <github@shard.dk>
2019-01-29 19:22:48 +0100

    Arduino - Mapping Incomming MIDI channel to state1 channel

--- Arduino/AJ_Johansen_Midi2CV_EncoderLED_RevA/AJ_Johansen_Midi2CV_EncoderLED_RevA.ino
+++ Arduino/AJ_Johansen_Midi2CV_EncoderLED_RevA/AJ_Johansen_Midi2CV_EncoderLED_RevA.ino
@@ -11,36 +11,7 @@
 */
 #include "midi.h";
 
-#define SWpin A0 //add 10nF to GND for debounce (not essential
-#define Apin A1 //add 47nF/100nF to GND for debounce
-#define Bpin A2 //add 47nF/100nF to GND for debounce
-#define CV3in A3
-#define SDA A4
-#define SCL A5
-#define CV6in A6
-#define CV7in A7
 
-#define LED1 2
-#define LED2 3
-#define LED3 4
-#define LED4 5
-#define LEDsel 10
-#define LEDsel2 11
-
-#define Gate1 6
-#define Gate2 7
-#define Gate3 8
-#define Gate4 9
-
-#define RDY-BSY 12
-#define LDAC 13
-
-boolean A_old=0, B_old=1, SW_old=0;
-boolean left = false, right = false, button = false;
-
-boolean LEDData[4][4];
-boolean LEDSelData = false, LEDSel2Data = false; 
-unsigned int encoder[4], buttoncnt = 0, state = 0;
 /* =====================================================
 ======================================================*/  
 void setupLEDData(){
@@ -104,13 +75,13 @@ void setup(){
 ======================================================*/  
 
 void loop(){
-//  if (left||right||button){
-  checkencoder();
+  checkencoder();                  //Check encoder and update values
 
-  updateLED();
+  updateLED();                     //LED on for active state    
   midiRead();
-  updateLEDValue(midi_channel, 0);
-  clearLED();
+//  updateLEDValue(midi_channel, 0); //Update current midi chan for state 0
+  updateLEDValue(MIDI_CH[0], 0); //Update current midi chan for state 0
+  clearLED();                      //LED off (dim light)
   
 }
 /* =====================================================
@@ -157,12 +128,18 @@ void checkencoder(){
   if (left){
     left = false;
     if (encoder[state] != 0) encoder[state]--;
-    if (state != 0) updateLEDValue(encoder[state] >> 2, state);
+    if (state != 0) {
+      MIDI_CH[state]=encoder[state] >> 2;
+      updateLEDValue(MIDI_CH[state], state);
+    }
   }
   if (right){
     right = false;
     if (encoder[state] < 0x3f) encoder[state]++;
-    if (state != 0) updateLEDValue(encoder[state] >> 2, state);
+    if (state != 0) {
+      MIDI_CH[state]=encoder[state] >> 2;
+      updateLEDValue(MIDI_CH[state], state);
+    }
   }
   if (button){
     button = false;    
--- Arduino/AJ_Johansen_Midi2CV_EncoderLED_RevA/midi.h
+++ Arduino/AJ_Johansen_Midi2CV_EncoderLED_RevA/midi.h
@@ -1,8 +1,6 @@
+#include "globals.h"
 #include <MIDI.h>  // Add Midi Library
 MIDI_CREATE_DEFAULT_INSTANCE();
-
-byte midi_channel=4;
-void MyHandleNoteOn(byte channel, byte pitch, byte velocity);
 
 void setupMidi(){
   MIDI.begin(MIDI_CHANNEL_OMNI); // Initialize the Midi Library.
@@ -11,6 +9,7 @@ void setupMidi(){
   MIDI.setHandleNoteOn(MyHandleNoteOn); // This is important!! This command
   // tells the Midi Library which function I want called when a Note ON command
   // is received. in this case it's "MyHandleNoteOn".
+  MIDI.setHandleNoteOff(MyHandleNoteOff);
 }
 
 byte midiRead(){
@@ -18,6 +17,18 @@ byte midiRead(){
 //  return midi_channel;
 }
 
+void ForwardMidiEvent(byte channel_in, byte channel_out, byte pitch, byte velocity) {
+  digitalWrite(LED4, HIGH);
+
+  delay(300);
+  
+  digitalWrite(LED4, LOW);
+
+  delay(300);
+
+  digitalWrite(LED4, HIGH);
+
+}
 // Below is my function that will be called by the Midi Library
 // when a MIDI NOTE ON message is received.
 // It will be passed bytes for Channel, Pitch, and Velocity
@@ -28,8 +39,8 @@ void MyHandleNoteOn(byte channel, byte pitch, byte velocity) {
   int gateVoltage = 0; // used for debugging
   int velVal = 0;
   
-  midi_channel=channel;
-
+  MIDI_CH[0]=channel;
+  if (MIDI_CH[0] == MIDI_CH[1]) ForwardMidiEvent(MIDI_CH[1], MIDI_CH[2], pitch, velocity);
 /*  DACnum = pitches[pitch-36]; //-36 to transpose bottom of CASIO keyboard to C0.
   dac.setVoltage(DACnum, false);
   Serial.print("channel: ");
@@ -68,4 +79,14 @@ void MyHandleNoteOn(byte channel, byte pitch, byte velocity) {
   Serial.print(", velVoltage: ");
 //  gateVoltage = analogRead(GATECHK); // voltage of GATE signal, used for debugging
   Serial.println(gateVoltage);*/
+}
+
+void MyHandleNoteOff(byte channel, byte pitch, byte velocity) {
+  int DACnum = 0;
+  int GATEval = LOW;
+  int gateVoltage = 0; // used for debugging
+  int velVal = 0;
+  
+  MIDI_CH[0]=channel;
+  if (MIDI_CH[0] == MIDI_CH[1]) ForwardMidiEvent(MIDI_CH[1], MIDI_CH[2], pitch, velocity);
 }
